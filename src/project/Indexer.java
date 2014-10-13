afbebdf59bb93524a09c7642b21d5d4f53a55fde
Ben Ng <ben.nsng@gmail.com>
2014-10-14 02:00:48 +0800

    index

--- src/project/Indexer.java
+++ src/project/Indexer.java
@@ -1,19 +1,25 @@
 package project;
 
 import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import org.htmlparser.beans.StringBean;
 
 public class Indexer {
 
 	private Spider spider;
 	private StopStem stopStem;
-	private InvertedIndex index;
+	private InvertedIndex pageIndex;
+	private InvertedIndex wordIndex;
 	
 	public Indexer() {
 		
 		try {
 			spider = new Spider("http://www.cse.ust.hk/", 30);
 			stopStem = new StopStem("stopwords.txt");
-			index = spider.Index();
+			pageIndex = spider.Index();
+			wordIndex = new InvertedIndex("idWord", "word");
 		}
 		catch (IOException ioe) {
 			ioe.printStackTrace ();
@@ -29,15 +35,33 @@ public class Indexer {
 		}
 	}
 	
-	public void Index() {
+	public void Index() throws IOException {
 		
-		
+		StringBean sb = new StringBean ();
+		Vector<String> v_str = new Vector<String>();
+		//while (pageIndex.getItr()) {
+			boolean links = false;
+			sb.setLinks (links);
+			//sb.setURL (url);
+			String temp = sb.getStrings();
+			String temp2 = "";
+			int wordCount = 0;
+			StringTokenizer st = new StringTokenizer(temp, "\n");
+			while(st.hasMoreTokens()){
+				temp2 = st.nextToken();
+				if(!v_str.contains(temp2)) {
+					v_str.add(temp2);
+					wordIndex.addEntry(++wordCount + "", temp2);
+				}
+	        }
+		//}
 	}
 	
 	public void SaveToJDBM() {
 		
 		try {
-			index.finalize();
+			pageIndex.finalize();
+			wordIndex.finalize();
 		}
 		catch(IOException ioe) {
 			ioe.printStackTrace();
