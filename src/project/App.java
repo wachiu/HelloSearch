ddfa632780bac2af7552444c1c3400add947dd6a
Ben Ng <ben.nsng@gmail.com>
2014-10-21 23:20:28 +0800

    code test

--- src/project/App.java
+++ src/project/App.java
@@ -1,6 +1,9 @@
 package project;
 
 import java.io.IOException;
+import java.util.LinkedList;
+
+import jdbm.helper.FastIterator;
 
 public class App {
 
@@ -10,7 +13,7 @@ public class App {
 	public App() {
 		indexer = new Indexer();
 		try {
-			spider = new Spider("http://www.cse.ust.hk/", 100, indexer);
+			spider = new Spider("http://www.cse.ust.hk/", 30, indexer);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -31,10 +34,65 @@ public class App {
 		indexer.finalize();
 	}
 	
+	public static void test() {
+		try {
+			//InvertedIndex index = new InvertedIndex("idUrl", "ht1");
+			//InvertedIndex index = new InvertedIndex("urlId", "ht1");
+			//InvertedIndex index = new InvertedIndex("titleId", "ht1");
+			//InvertedIndex index = new InvertedIndex("idTitle", "ht1");
+			InvertedIndex index = new InvertedIndex("bodyId", "ht1");
+			
+			FastIterator iter = index.getIteratorVals();
+			Object obj;
+			Word w;
+			urlInfo u;
+			
+			for(int i = 0; i < 2000 && (obj = iter.next()) != null; i++) {
+				//1.
+				//System.out.println(obj);
+				//u = (urlInfo)obj;
+				//System.out.println(u.key + "," + u.url + "," + u.title + "," + u.lastModified + "," + u.size + "," + u.parent + "," + u.children);
+				
+				//2.
+				//System.out.println(obj);
+
+				//3.
+				//System.out.println(obj);
+				
+				//4.
+				//LinkedList<Posting> p = ((Word)obj).getPosting();
+				//System.out.println(p.get(0).getDocumentId());
+				//System.out.println(p.get(0).getPositions());
+				//System.out.println(p.get(1).getDocumentId());
+				//System.out.println(p.get(1).getPositions());	
+
+				//5.
+				System.out.println(obj);
+				
+				//6.
+				//LinkedList<Posting> p = ((Word)obj).getPosting();
+				//System.out.println(((Word)obj).getWord());
+				//System.out.println(p.get(0).getDocumentId());
+				//System.out.println(p.get(0).getPositions());
+				//System.out.println(p.get(1).getDocumentId());
+				//System.out.println(p.get(1).getPositions());	
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
 	public static void main (String[] args) {
+		//*//
 		App app = new App();
 		app.run();
 		app.finalize();
+		/*/
+		App.test();
+		//*/
 	}
 	
 }
