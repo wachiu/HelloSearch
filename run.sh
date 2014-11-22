a9e02121a1ca9f740bf21a3fb1db8f19ce2aa2d3
Ben Ng <ben.nsng@gmail.com>
2014-11-22 12:34:54 +0800

    update run script

--- run.sh
+++ run.sh
@@ -1,6 +1,19 @@
-cd bin && jar -cvfm ../executable/app.jar manifest/crawler_manifest.txt IRUtilities/*.class project/*.class org/json/*.class
-cd ../executable && java -jar app.jar search "check"
-#cd ../executable && java -jar app.jar index
-cd ..
-chmod 777 executable/*.db
-chmod 777 executable/*.lg
+if [ "$1" == "compile" ]
+then
+	cd bin && jar -cvfm ../executable/app.jar manifest/crawler_manifest.txt IRUtilities/*.class project/*.class org/json/*.class
+	cd ..
+elif [ "$1" == "index" ]
+then
+	cd executable && java -jar app.jar index
+        cd ..
+        chmod 777 executable/*.db
+        chmod 777 executable/*.lg
+elif [ "$1" == "search" ]
+then
+	cd executable && java -jar app.jar search "$2"
+	cd ..
+elif [ "$1" == "rmdb" ]
+then
+	rm executable/*.db executable/*.lg
+fi
+
